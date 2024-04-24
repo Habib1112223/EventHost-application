ab075b2a5af132b1584c23bfe2ec86f07a6d0b86
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2024-04-24 10:02:29 +0600

    first commit

---
+++ src/Home page/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+
+
+const Banner = () => {
+      return (
+            <div>
+                  <div>
+                        <img className="p-2 pl-5 pr-10 mt-10 mx-auto" src="https://i.ibb.co/w0Q2rzG/Rectangle-8.png" alt="" />
+                       
+                  </div>
+                  <div className="p-2 ml-44 -mt-72 text-white ">
+                        <h1 className="text-4xl font-bold ">CMA Fest</h1>
+                        <p>Make Memories that will last a lifetime.see your favorite <br /> Artist Life with CMA Fest. 
+                               </p>
+                               <button className="btn-primary"> See tickets</button>
+                      
+                  </div>
+            </div>
+      );
+};
+
+export default Banner;
---
+++ src/Home page/Events/Events.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const div = () => {
+    return (
+        <div>
+            <div>
+                <h2>this is a events</h2>
+            </div>
+        </div>
+    );
+};
+
+export default div;
